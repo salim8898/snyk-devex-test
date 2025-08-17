1b280880048768b6209742590fb5fa1e74bd4596
Salim Shaikh <43460517+salim8898@users.noreply.github.com>
2025-08-17 15:42:22 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+
+def unsafe_eval(user_code):
+    # Code injection vulnerability
+    eval(user_code)
+
+def weak_crypto():
+    # Weak cryptography
+    import hashlib
+    return hashlib.md5(b"password").hexdigest()
